f2de32b13e0f2a211c465f1014d1787f3bef60bd
slehman133 <43526258+slehman133@users.noreply.github.com>
2024-02-04 16:26:56 -0800

    some changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,112 +2,59 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="flex flex-row justify-between my-3 mx-10 text-xl font-mono">
+        <div>
+          <a href="/">Home</a>
+        </div>
+        <div className="flex justify-evenly gap-10">
+          <a href="#description">About</a>
+          <a href="#">Lorem</a>
+          <a href="#">Lorem</a>
+          <a href="#">Lorem</a>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24 mx-32">
+        <section id="home">
+          <div className="flex flex-col items-center">
+            <h1 className="font-bold text-6xl">Ecommerce Website for Parasol Laboratories</h1>
+            <div>
+              <Image src={"/logo.png"} alt="Parasol Labs logo" height={200} width={200} />
+            </div>
+            <p>A project for CS426 - Senior Projects, at the University of Nevada, Reno</p>
+            <p>Team 21: Kaeden Carver, Nicholas Whitehorn, Samuel Lehman</p>
+            <p>Instructor(s): Dave Feil Seifer, Sara Davis, Devrin Lee, Vinh Le</p>
+            <p>External Advisor(s): Erin Keith: UNR CSE Instructor, Ahmed Munoz: CEO of Parasol Labs</p>
+          </div>
+        </section>
+        <section id="description">
+          <div className="my-2">
+            <h3 className="font-semibold text-4xl">Project Description</h3>
+            <p className="py-2">
+              Team 21`s main goal with the ecommerce website for Parasol Laboratories Inc.is to build a platform
+              with a sleek front-end design that will help promote Parasol Labs and inform potential customers and
+              investors about services and products they offer to provide. To accomplish this goal, Team 21 has chosen
+              to go with the tech stack of Next.js, TypeScript, Tailwind CSS, Prima, and PostgreSQL because these technologies
+              are widely praised for their performance, ease of use, and reliability. The main functionalities of the web
+              application are to be able to create and edit blog or news posts to the news section of the website, the ability
+              to sell products and services that Parasol Labs is offering, tracking website analytics, and providing a nice user
+              experience. To achieve these goals we will be using Sanity to manage all functionalities regarding news, Shopify to
+              provide a place to store product information as well as handling the payment processing in a secure manner, Google
+              Analytics to track information regarding user`s visiting the website, and a combination of custom CSS, CSS libraries
+              such as Tailwind CSS,  a front-end component library such as React to provide a single page application experience
+              as well as front-end component libraries such as NextUI and DaisyUI.
+            </p>
+            <p className="py-2">
+              To provide Parasol Labs with a website that is reliable, the team is using Vercel to host the website as they boast
+              a 99.99% uptime on their services. In terms of security, we will be using a library called NextAuth to handle user
+              authentication as well as managing sessions. We also implemented functionality to salt and hash passwords in our
+              database so if it is compromised no user`s passwords will be discovered. In regards to safety, team 21 has made
+              sure to not develop any features or functionalities that would be detrimental or cause harm on to individuals or
+              the public.
+            </p>
+          </div>
+        </section>
+      </main >
+    </>
   );
 }
