955a21d835197af94e9bc8a4667cf039b0e51e2d
slehman133 <43526258+slehman133@users.noreply.github.com>
2024-02-06 11:07:46 -0800

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/x-icon" href="/images/favicon.ico"></link>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
